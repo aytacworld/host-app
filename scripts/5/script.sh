1af3466638856207b0eb0ee952ad1a956737f1af
Adem Aytaç <adem@aytacworld.com>
2018-04-08 16:33:32 +0200

    script message updated

--- scripts/5/script.sh
+++ scripts/5/script.sh
@@ -26,10 +26,17 @@ cd ~ && sudo cp -r host-main-app/ /var/app/host-app/
 echo "Install pm2 and setup host-app"
 npm i -g pm2
 pm2 start /var/app/host-app/host-app-start.js --name "host-app"
+
+# Add startup script for pm2
+echo "Startup script for pm2 will be added"
+echo "This script will return an error, but ignore it, the script will be there"
+echo "Just reboot when the script finishes"
+read -p "Press enter to continue"
+
 pm2 startup
 sudo env PATH=$PATH:${NVM_BIN} ${NVM_BIN}/../lib/node_modules/pm2/bin/pm2 startup systemd -u ${USERNAME} --hp ${HOME}
 
-cd ~
-
-echo "Reboot machine"
-# sudo reboot
+echo "You will see this message, if the previous script didn't exited with error code"
+echo "So machine will be rebooted"
+read -p "Press enter to reboot"
+sudo reboot
