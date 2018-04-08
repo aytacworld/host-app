847fbbad9d29a760fcb90de62d283eda803afeb8
Adem Aytaç <adem@aytacworld.com>
2018-04-08 14:14:15 +0200

    ipfix

--- setup-scripts/part-1/script.sh
+++ setup-scripts/part-1/script.sh
@@ -10,7 +10,7 @@ read -p "Please enter your hostname(or server name):(app) " HOSTNAME
 read -p "Please enter your domain:(example.com) " DOMAIN
 read -p "Please enter a username (who will become a sudo user): " USERNAME
 FQDN=${HOSTNAME}.${DOMAIN}
-IP_ADDRESS=$(ifconfig | awk '/inet /{print substr($2,0)}' | grep -v 127.0.0.10)
+IP_ADDRESS=$(ifconfig | awk '/inet /{print substr($2,0)}' | grep -v 127.0.0.1)
 
 # Set hostname
 echo "Updating hostname"
