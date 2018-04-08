eca8016c90fda9570df408a9bceed2511a191418
Adem Aytaç <adem@aytacworld.com>
2018-04-08 15:49:21 +0200

    ssl setup fix

--- scripts/3/script.sh
+++ scripts/3/script.sh
@@ -23,11 +23,15 @@ sudo cp ${PART_SETUP_DIR}/nginx.conf /etc/nginx/nginx.conf
 # create default server block
 echo "Updating default.conf"
 sed -i -e 's/${FQDN}/'"${FQDN}"'/g' ${PART_SETUP_DIR}/nginx-default.conf
-sudo cp ${PART_SETUP_DIR}/nginx-default.conf /etc/nginx/sites-available/default.conf
+sudo cp ${PART_SETUP_DIR}/nginx-default-before-ssl.conf /etc/nginx/sites-available/default.conf
 
 # setup certbot
 echo "Generating ssl"
 sudo certbot --nginx certonly --agree-tos --register-unsafely-without-email -d ${FQDN}
+
+# Copy ssl version of defaul conf
+echo "Copy ssl version of default.conf"
+sudo cat ${PART_SETUP_DIR}/nginx-default-after-ssl.conf > /etc/nginx/sites-available/default.conf
 
 # restart nginx
 echo "Restart nginx"
